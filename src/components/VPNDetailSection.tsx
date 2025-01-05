6393eae677ecc7083962f50949e1fd35a7793205
GergulasBalint <92796917+GergulasBalint@users.noreply.github.com>
2025-01-05 14:58:26 +0100

    made it thicc

--- src/components/VPNDetailSection.tsx
+++ src/components/VPNDetailSection.tsx
@@ -21,7 +21,7 @@ const VPNDetailSection: React.FC<VPNDetailSectionProps> = ({ vpn }) => {
   
   return (
     <div className={`relative bg-white/5 backdrop-blur-sm rounded-lg p-6 mb-8 ${
-      isNordVPN ? 'border-2 border-yellow-400/80 shadow-[0_0_15px_rgba(251,191,36,0.3)]' : ''
+      isNordVPN ? 'border-4 border-yellow-400/90 shadow-[0_0_25px_rgba(251,191,36,0.5)]' : ''
     }`}>
       <div className="flex flex-col md:flex-row gap-6">
         <div className="md:w-1/4 relative">
